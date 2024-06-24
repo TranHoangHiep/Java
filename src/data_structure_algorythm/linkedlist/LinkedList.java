5d4e5d06b0979e7e7da62b2c092b6220dc70d953
Hoang Hiep <hoanghiep@Hoangs-Mac-mini.local>
2024-06-24 14:32:27 +0700

    Linked list data structure

---
+++ src/data_structure_algorythm/linkedlist/LinkedList.java
@@ -0,0 +1,45 @@
+package data_structure_algorythm.linkedlist;
+
+public class LinkedList {
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void getHead() {
+        System.out.println("Head: " + head.value);
+    }
+
+    public void getTail() {
+        System.out.println("Tail: " + tail.value);
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+
+    public void printLinkedList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+}
---
+++ src/data_structure_algorythm/linkedlist/TestLinkedList.java
@@ -0,0 +1,13 @@
+package data_structure_algorythm.linkedlist;
+
+public class TestLinkedList {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList(4);
+        linkedList.printLinkedList();
+
+        linkedList.getHead();
+        linkedList.getTail();
+        linkedList.getLength();
+
+    }
+}
