a7ce8080035933897a9b52e9aaac4ed57bfd5904
Hoang Hiep <hoanghiep@Hoangs-Mac-mini.local>
2024-06-26 22:19:57 +0700

    DoubleLinkedList: insert item

--- src/data_structure_algorythm/linkedlist/DoublyLinkedList.java
+++ src/data_structure_algorythm/linkedlist/DoublyLinkedList.java
@@ -124,4 +124,25 @@ public class DoublyLinkedList {
         }
         return false;
     }
+
+    public boolean insert(int index, int value) {
+        if (index < 0 || index > length) return false;
+        if (index == 0) {
+            prepend(value);
+            return true;
+        }
+        if (index == length) {
+            append(value);
+            return true;
+        }
+        Node newNode = new Node(value);
+        Node before = get(index-1);
+        Node after = before.next;
+        before.next = newNode;
+        newNode.prev = before;
+        newNode.next = after;
+        after.prev = newNode;
+        length++;
+        return true;
+    }
 }
