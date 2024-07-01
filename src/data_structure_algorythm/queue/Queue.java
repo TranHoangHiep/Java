68a4cfa75774e04b3ed3820bfebf94e4c34dac01
Hoang Hiep <hoanghiep@Hoangs-Mac-mini.local>
2024-07-01 23:20:30 +0700

    Queue: constructor

---
+++ src/data_structure_algorythm/queue/Queue.java
@@ -0,0 +1,44 @@
+package data_structure_algorythm.queue;
+
+import data_structure_algorythm.stack.Stack;
+
+public class Queue {
+    class Node {
+        int value;
+        Node next;
+        Node (int value) {
+            this.value = value;
+        }
+    }
+
+    private Node first;
+    private Node last;
+    private int length;
+
+    public Queue(int value) {
+        Node newNode = new Node(value);
+        first = newNode;
+        last = newNode;
+        length = 1;
+    }
+
+    public void getFirst() {
+        System.out.println("First: " + first.value);
+    }
+
+    public void getLast() {
+        System.out.println("Last: " + last.value);
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+
+    public void printQueue() {
+        Node temp = first;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+}
