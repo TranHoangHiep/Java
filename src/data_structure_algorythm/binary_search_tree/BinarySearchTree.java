d4d17c7422c2c84d16f18d38b758437db69c379f
Hoang Hiep <hoanghiep@Hoangs-Mac-mini.local>
2024-07-08 23:07:35 +0700

    BST: insert

--- src/data_structure_algorythm/binary_search_tree/BinarySearchTree.java
+++ src/data_structure_algorythm/binary_search_tree/BinarySearchTree.java
@@ -11,4 +11,28 @@ public class BinarySearchTree {
     }
     Node root;
 
+    public boolean insert(int value) {
+        Node newNode = new Node(value);
+        if (root == null) {
+            root = newNode;
+            return true;
+        }
+        Node temp = root;
+        while(true) {
+            if (temp.value == newNode.value) return false;
+            if (newNode.value < temp.value) {
+                if (temp.left == null) {
+                   temp.left = newNode;
+                   return true;
+                }
+                temp = temp.left;
+            } else {
+                if (temp.right == null) {
+                    temp.right = newNode;
+                    return true;
+                }
+                temp = temp.right;
+            }
+        }
+    }
 }
