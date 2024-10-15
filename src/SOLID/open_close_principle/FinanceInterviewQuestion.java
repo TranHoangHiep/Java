97451d37f4cf13b8ec460002d86c52c97473f5a3
Hoang Hiep <hoanghiep@Hoangs-Mac-mini.local>
2024-10-15 23:28:34 +0700

    Design pattern: Open close principle

---
+++ src/SOLID/open_close_principle/FinanceInterviewQuestion.java
@@ -0,0 +1,9 @@
+package SOLID.open_close_principle;
+
+public class FinanceInterviewQuestion implements InterviewQuestion {
+
+    @Override
+    public void execute() {
+        System.out.println("FinanceInterviewQuestion...");
+    }
+}
---
+++ src/SOLID/open_close_principle/Test.java
@@ -0,0 +1,8 @@
+package SOLID.open_close_principle;
+
+public class Test {
+    public static void main(String[] args) {
+        InterviewQuestionProcessor processor = new InterviewQuestionProcessor();
+        processor.process(new AlgorithmsInterviewQuestion());
+    }
+}
