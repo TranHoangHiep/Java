f0a30b5455195709c9ec4cb1b4148d3b987f3c09
Hoang Hiep <hoanghiep@Mac.lan>
2025-01-21 00:10:33 +0700

    longest substtring

---
+++ src/data_structure_algorythm/sliding_window/LongestSubstring.java
@@ -0,0 +1,16 @@
+package data_structure_algorythm.sliding_window;
+
+public class LongestSubstring {
+    public static void main(String[] args) {
+        // Longest substring without repeating Characters
+        // Given a string s, find the length of the longest substring without repeating characters.
+        String s = "abcabcbb";
+        System.out.println("Length of the longest substring without repeating characters: " + lengthOfLongestSubstring(s));
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        
+    }
+
+
+}
